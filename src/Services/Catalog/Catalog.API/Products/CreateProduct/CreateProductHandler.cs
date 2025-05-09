b2e994f4ed8482158a6c6d0bc24b0b2625d9ac50
rizwan <rizwan.ahmad.khan>
2025-05-09 21:56:57 +0500

    seting up and running  docker

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,12 +1,11 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
+﻿
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, string Description, decimal Price, string ImageFile, List<string> Categories)
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -25,9 +24,10 @@ namespace Catalog.API.Products.CreateProduct
             };
 
             //save to database
-
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
             //return result
-            return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
